59199f3fd99c64ee4948b878ab8816202f77d865
김경석 <0xe82de@gmail.com>
2022-04-25 18:50:13 +0900

    #S06P31A106-57 Create: 메인 클래스 추가

---
+++ backend/src/test/java/com/i3e3/mindlet/MindletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.i3e3.mindlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
