f3be011e8bd612ff9e4e0316fe2e8db1d991491a
김경석 <0xe82de@gmail.com>
2022-04-27 17:16:08 +0900

    #S06P31A106-72 Feat: 회원 민들레 조회 이력 Entity 개발

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/entity/MemberDandelionHistory.java
@@ -0,0 +1,33 @@
+package com.i3e3.mindlet.domain.member.entity;
+
+import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_member_dandelion_history")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@IdClass(MemberDandelionHistorySeq.class)
+public class MemberDandelionHistory {
+
+    @Id
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "member_seq", columnDefinition = "BIGINT UNSIGNED")
+    private Member member;
+
+    @Id
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "dandelion_seq", columnDefinition = "BIGINT UNSIGNED")
+    private Dandelion dandelion;
+
+    @Builder
+    public MemberDandelionHistory(Member member, Dandelion dandelion) {
+        this.member = member;
+        this.dandelion = dandelion;
+
+        member.getMemberDandelionHistories().add(this);
+        dandelion.getMemberDandelionHistories().add(this);
+    }
+}
