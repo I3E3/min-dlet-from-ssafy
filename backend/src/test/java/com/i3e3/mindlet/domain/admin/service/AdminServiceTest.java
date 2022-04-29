b561e31b24b776513d9686ce1cbe6cf4a29d0cdc
김경석 <0xe82de@gmail.com>
2022-04-29 19:29:10 +0900

    #S06P31A106-76 Test: 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/admin/service/AdminServiceTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/admin/service/AdminServiceTest.java
@@ -3,6 +3,7 @@ package com.i3e3.mindlet.domain.admin.service;
 import com.i3e3.mindlet.domain.admin.controller.form.RegisterForm;
 import com.i3e3.mindlet.domain.admin.entity.Admin;
 import com.i3e3.mindlet.domain.admin.entity.RegisterKey;
+import com.i3e3.mindlet.domain.admin.exception.DuplicateIdException;
 import com.i3e3.mindlet.domain.admin.exception.PasswordContainIdException;
 import com.i3e3.mindlet.domain.admin.repository.AdminRepository;
 import com.i3e3.mindlet.domain.admin.repository.RegisterKeyRepository;
@@ -110,5 +111,39 @@ class AdminServiceTest {
         assertThatThrownBy(() -> adminService.register(newRegisterForm.toDto()))
                 .isInstanceOf(PasswordContainIdException.class)
                 .hasMessage(ErrorMessage.PASSWORD_CONTAIN_ID.getMessage());
+    }
+
+    @Test
+    @DisplayName("관리자 회원가입 실패 - 아이디가 중복된 경우")
+    void registerFailWhenDuplicateId() {
+        /**
+         * 임시 키 생성
+         */
+        registerKeyRepository.save(RegisterKey.builder()
+                .value(registerForm1.getKey())
+                .build());
+        em.flush();
+        em.clear();
+
+        /**
+         * 회원가입
+         */
+        adminService.register(registerForm1.toDto());
+
+        /**
+         * 이미 회원가입된 계정의 아이디로 회원가입 시도
+         */
+        RegisterForm newRegisterForm = RegisterForm.builder()
+                .id(registerForm1.getId())
+                .password("pass12#$")
+                .key("newKey")
+                .build();
+
+        /**
+         * 예외 발생 검증
+         */
+        assertThatThrownBy(() -> adminService.register(newRegisterForm.toDto()))
+                .isInstanceOf(DuplicateIdException.class)
+                .hasMessage(ErrorMessage.DUPLICATE_ID.getMessage());
     }
 }
