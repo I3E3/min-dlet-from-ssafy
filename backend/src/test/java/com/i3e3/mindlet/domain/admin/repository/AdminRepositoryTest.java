6c42554fc3da29c7543fd7f3bcc7c6340c2efcca
김경석 <0xe82de@gmail.com>
2022-04-29 19:27:26 +0900

    #S06P31A106-76 Create: 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/admin/repository/AdminRepositoryTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/admin/repository/AdminRepositoryTest.java
@@ -3,11 +3,15 @@ package com.i3e3.mindlet.domain.admin.repository;
 import com.i3e3.mindlet.domain.admin.entity.Admin;
 import com.i3e3.mindlet.global.enums.Role;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -32,5 +36,29 @@ class AdminRepositoryTest {
                 .password("pass12#$")
                 .role(Role.ADMIN)
                 .build();
+    }
+
+    @Test
+    @DisplayName("관리자 엔티티 조회 by 식별키 - 성공")
+    void findAdminBySeqSuccess() {
+        /**
+         * 엔티티 저장 후 영속성 컨텍스트 초기화
+         */
+        Admin savedAdmin = adminRepository.save(admin1);
+        em.flush();
+        em.clear();
+
+        /**
+         * 데이터 조회
+         */
+        Admin findAdmin = adminRepository.findAdminBySeq(savedAdmin.getSeq())
+                .orElse(null);
+
+        /**
+         * 데이터 검증
+         */
+        assertThat(findAdmin).isNotNull();
+        assertThat(findAdmin.getId()).isEqualTo(admin1.getId());
+        assertThat(findAdmin.getPassword()).isEqualTo(admin1.getPassword());
     }
 }
