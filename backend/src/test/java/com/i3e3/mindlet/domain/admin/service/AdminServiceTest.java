188e00c59eabea965a020240db02ef1afc8baf6c
김경석 <0xe82de@gmail.com>
2022-04-29 19:28:43 +0900

    #S06P31A106-76 Test: 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/admin/service/AdminServiceTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/admin/service/AdminServiceTest.java
@@ -1,15 +1,21 @@
 package com.i3e3.mindlet.domain.admin.service;
 
 import com.i3e3.mindlet.domain.admin.controller.form.RegisterForm;
+import com.i3e3.mindlet.domain.admin.entity.Admin;
+import com.i3e3.mindlet.domain.admin.entity.RegisterKey;
 import com.i3e3.mindlet.domain.admin.repository.AdminRepository;
 import com.i3e3.mindlet.domain.admin.repository.RegisterKeyRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -44,5 +50,35 @@ class AdminServiceTest {
                 .password("password01")
                 .key("aaaa-bbbb-cccc-dddd")
                 .build();
+    }
+
+    @Test
+    @DisplayName("관리자 회원가입 - 성공")
+    void registerSuccess() {
+        /**
+         * 임시 키 생성
+         */
+        registerKeyRepository.save(RegisterKey.builder()
+                .value(registerForm1.getKey())
+                .build());
+        em.flush();
+        em.clear();
+
+        /**
+         * 회원가입
+         */
+        Admin savedAdmin = adminService.register(registerForm1.toDto());
+
+        /**
+         * 관리자 데이터 조회
+         */
+        Admin findAdmin = adminRepository.findAdminBySeq(savedAdmin.getSeq())
+                .orElse(null);
+
+        /**
+         * 데이터 검증
+         */
+        assertThat(findAdmin.getId()).isEqualTo(registerForm1.getId());
+        assertThat(passwordEncoder.matches(registerForm1.getPassword(), findAdmin.getPassword())).isTrue();
     }
 }
