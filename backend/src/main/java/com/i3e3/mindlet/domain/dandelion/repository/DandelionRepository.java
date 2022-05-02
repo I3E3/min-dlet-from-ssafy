497c600c7ebcab257959d0549261f8062b9d874c
김경석 <0xe82de@gmail.com>
2022-05-02 18:11:28 +0900

    #S06P31A106-46 Create: 민들레 레파지토리 인터페이스 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepository.java
@@ -0,0 +1,7 @@
+package com.i3e3.mindlet.domain.dandelion.repository;
+
+import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DandelionRepository extends JpaRepository<Dandelion, Long> {
+}
