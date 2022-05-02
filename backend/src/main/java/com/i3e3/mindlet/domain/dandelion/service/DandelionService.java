a3713ab0b3f38c475c7541c513bab38bfa6a46fa
김경석 <0xe82de@gmail.com>
2022-05-02 18:10:02 +0900

    #S06P31A106-46 Feat: 민들레 상태(Blossomed) 확인 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionService.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionService.java
@@ -1,4 +1,6 @@
 package com.i3e3.mindlet.domain.dandelion.service;
 
 public interface DandelionService {
+
+    boolean isBlossomed(Long dandelionSeq);
 }
--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceImpl.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceImpl.java
@@ -1,5 +1,8 @@
 package com.i3e3.mindlet.domain.dandelion.service;
 
+import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
+import com.i3e3.mindlet.domain.dandelion.repository.DandelionRepository;
+import com.i3e3.mindlet.global.constant.message.ErrorMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +13,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class DandelionServiceImpl implements DandelionService {
+
+    private final DandelionRepository dandelionRepository;
+
+    @Override
+    public boolean isBlossomed(Long dandelionSeq) {
+        Dandelion findDandelion = dandelionRepository.findBySeq(dandelionSeq)
+                .orElseThrow(() -> new IllegalStateException(ErrorMessage.INVALID_REQUEST.getMessage()));
+
+        return findDandelion.getStatus() == Dandelion.Status.BLOSSOMED;
+    }
 }
