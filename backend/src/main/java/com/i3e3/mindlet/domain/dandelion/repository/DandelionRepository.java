84043a22ef71c5b9da4b7e7558cbb09defe0b319
김경석 <0xe82de@gmail.com>
2022-05-02 18:11:51 +0900

    #S06P31A106-46 Feat: 식별키로 민들레 조회 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepository.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepository.java
@@ -2,6 +2,13 @@ package com.i3e3.mindlet.domain.dandelion.repository;
 
 import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface DandelionRepository extends JpaRepository<Dandelion, Long> {
+
+    @Query("SELECT d FROM Dandelion d WHERE d.seq = :seq AND d.isDeleted = FALSE")
+    Optional<Dandelion> findBySeq(@Param("seq") Long seq);
 }
