a1c18ba4db2c374b628b8a2c35672fa785668a26
김경석 <0xe82de@gmail.com>
2022-05-02 18:07:36 +0900

    #S06P31A106-46 Create: 민들레 컨트롤러 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/controller/DandelionController.java
@@ -0,0 +1,12 @@
+package com.i3e3.mindlet.domain.dandelion.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/dandelions")
+public class DandelionController {
+}
