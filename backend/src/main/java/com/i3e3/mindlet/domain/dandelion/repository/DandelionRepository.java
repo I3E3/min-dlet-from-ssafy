10c56f5293f566b7fe288c127b74e955c425b097
Yungi <luk951202@naver.com>
2022-05-03 17:26:22 +0900

    #S06P31A106-33 Refactor: 커스텀 인터페이스 상속

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepository.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
-public interface DandelionRepository extends JpaRepository<Dandelion, Long> {
+public interface DandelionRepository extends JpaRepository<Dandelion, Long>, DandelionRepositoryCustom {
 
     @Query("SELECT d FROM Dandelion d WHERE d.seq = :seq AND d.isDeleted = FALSE")
     Optional<Dandelion> findBySeq(@Param("seq") Long seq);
