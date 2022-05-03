c3b39e6b7e8060a624e1940f395849b95e469ae7
Yungi <luk951202@naver.com>
2022-05-03 17:59:29 +0900

    #S06P31A106-33 Test: 민들레 서비스 클래스 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceTest.java
@@ -274,4 +274,20 @@ class DandelionServiceTest {
         // then
         assertThat(seedCountDto.getLeftSeedCount()).isEqualTo(DandelionConst.MAX_USING_DANDELION_COUNT.getValue());
     }
+
+    @Test
+    @DisplayName("남은 씨앗 개수 조회 - hold 상태의 민들레가 1개 등록될 경우")
+    void countLeftSeedWhenHold() {
+        // given
+        dandelion1.changeStatus(Dandelion.Status.HOLD);
+        Member savedMember = memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        // when
+        SeedCountDto seedCountDto = dandelionService.getLeftSeedCount(savedMember.getSeq());
+
+        // then
+        assertThat(seedCountDto.getLeftSeedCount()).isEqualTo(DandelionConst.MAX_USING_DANDELION_COUNT.getValue() - 1);
+    }
 }
