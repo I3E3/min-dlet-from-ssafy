5c128615ed66d2808a2c18503012728b70499b12
SSAFY <gksrnrdmlsk@naver.com>
2022-05-03 11:44:13 +0900

    #S06P31A106-81 Chore: 배경 설정 및 index.tsx react18 버전 업데이트

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import './index.module.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+import { createRoot } from 'react-dom/client';
+
+const rootNode = document.getElementById('root')
+
+const root = createRoot(rootNode!)
+
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
