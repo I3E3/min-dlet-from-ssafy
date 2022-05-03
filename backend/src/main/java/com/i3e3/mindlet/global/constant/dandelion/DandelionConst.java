66e3f81e41ed6696d89a274b9ac78896aa199014
Yungi <luk951202@naver.com>
2022-05-03 17:32:46 +0900

    #S06P31A106-33 Create: 민들레 상수 enum 클래스 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/global/constant/dandelion/DandelionConst.java
@@ -0,0 +1,14 @@
+package com.i3e3.mindlet.global.constant.dandelion;
+
+public enum DandelionConst {
+
+    private final int value;
+
+    DandelionConst(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
