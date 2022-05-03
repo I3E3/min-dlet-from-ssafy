b7b47be71a87c9e21e51d0846b08caf4b14adb2f
Yungi <luk951202@naver.com>
2022-05-03 17:22:38 +0900

    #S06P31A106-33 Feat: 남은 씨앗 개수 조회용 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceImpl.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceImpl.java
@@ -2,6 +2,9 @@ package com.i3e3.mindlet.domain.dandelion.service;
 
 import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
 import com.i3e3.mindlet.domain.dandelion.repository.DandelionRepository;
+import com.i3e3.mindlet.domain.dandelion.service.dto.SeedCountDto;
+import com.i3e3.mindlet.domain.member.repository.MemberRepository;
+import com.i3e3.mindlet.global.constant.dandelion.DandelionConst;
 import com.i3e3.mindlet.global.constant.message.ErrorMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class DandelionServiceImpl implements DandelionService {
 
     private final DandelionRepository dandelionRepository;
+    private final MemberRepository memberRepository;
 
     @Override
     public boolean isBlossomed(Long dandelionSeq) {
@@ -40,5 +44,25 @@ public class DandelionServiceImpl implements DandelionService {
 
         findDandelion.changeDescription(description);
         return findDandelion.getDescription();
+    }
+
+    @Override
+    public SeedCountDto getLeftSeedCount(Long memberSeq) {
+
+        if (!memberRepository.existsBySeq(memberSeq)) {
+            throw new IllegalStateException(ErrorMessage.INVALID_REQUEST.getMessage());
+        }
+
+        Integer countUsingSeed = dandelionRepository.countUsingSeed(memberSeq);
+        int maxUsingDandelionCount = DandelionConst.MAX_USING_DANDELION_COUNT.getValue();
+        if (countUsingSeed > maxUsingDandelionCount) {
+            throw new IllegalStateException(ErrorMessage.MORE_THAN_MAX_COUNT.getMessage());
+        }
+
+        int leftSeedCount = maxUsingDandelionCount - countUsingSeed;
+
+        return SeedCountDto.builder()
+                .leftSeedCount(leftSeedCount)
+                .build();
     }
 }
