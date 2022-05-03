433d87a1ff65b8fdf287c395d7bb51cf195d8bd5
Yungi <luk951202@naver.com>
2022-05-03 17:41:56 +0900

    #S06P31A106-33 Test: 민들레 레포지토리 클래스 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepositoryTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepositoryTest.java
@@ -136,4 +136,20 @@ class DandelionRepositoryTest {
         // then
         assertThat(countUsingSeed).isEqualTo(0);
     }
+
+    @Test
+    @DisplayName("사용중인 민들레 개수 조회 - hold 상태의 민들레가 1개인 경우")
+    void countUsingSeedWhenHold() {
+        // given
+        Member savedMember = memberRepository.save(member1);
+        dandelion1.changeStatus(Dandelion.Status.HOLD);
+        dandelionRepository.save(dandelion1);
+        em.flush();
+        em.clear();
+
+        // when
+        int countUsingSeed = dandelionRepository.countUsingSeed(savedMember.getSeq());
+        // then
+        assertThat(countUsingSeed).isEqualTo(1);
+    }
 }
