d8a3b58db2d4bdf6604bb11a285ad57c32059ed6
sunmin <sunmlee126@gmail.com>
2022-05-04 16:02:55 +0900

    #S06P31A106-92 Refactor: 불필요한 코드 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,6 @@ import Router from 'routes/routes';
 import { BrowserView, MobileView } from 'react-device-detect';
 
 function App() {
-  function setScreenSize() {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-
-  useEffect(() => {
-    setScreenSize();
-  });
-
   return (
     <div>
       <RecoilRoot>
