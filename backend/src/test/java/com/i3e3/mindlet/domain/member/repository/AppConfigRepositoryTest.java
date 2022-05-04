6e9f6767657c22c0f4a41faef18180de6247ce03
안영진 <fufru@naver.com>
2022-05-04 18:27:29 +0900

    #S06P31A106-30 Create: 앱 설정 레포지토리 테스트 클래스 생성

---
+++ backend/src/test/java/com/i3e3/mindlet/domain/member/repository/AppConfigRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.i3e3.mindlet.domain.member.repository;
+
+import com.i3e3.mindlet.domain.member.entity.AppConfig;
+import com.i3e3.mindlet.domain.member.entity.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class AppConfigRepositoryTest {
+
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private AppConfigRepository appConfigRepository;
+
+    private Member member1;
+
+    private AppConfig appConfig1;
+
+    @BeforeEach
+    void setUp() {
+        memberRepository.deleteAll();
+        appConfigRepository.deleteAll();
+
+        em.flush();
+        em.clear();
+
+        member1 = Member.builder()
+                .id("아이디1")
+                .password("패스워드1")
+                .tel("010-1234-1234")
+                .build();
+
+        appConfig1 = AppConfig.builder()
+                .member(member1)
+                .language(AppConfig.Language.ENGLISH)
+                .build();
+    }
+}
