2080be4c2fbbc3287e9d1cacd5224cba2546b1d2
안영진 <fufru@naver.com>
2022-05-04 18:20:47 +0900

    #S06P31A106-30 Feat: 커뮤니티 변경 서비스 메소드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/member/service/MemberService.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.i3e3.mindlet.domain.member.service;
 
+import com.i3e3.mindlet.global.enums.Community;
+
 public interface MemberService {
+
+    void changeCommunity(Long memberSeq, Community community);
 
 }
--- backend/src/main/java/com/i3e3/mindlet/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/service/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package com.i3e3.mindlet.domain.member.service;
 
+import com.i3e3.mindlet.domain.member.entity.AppConfig;
+import com.i3e3.mindlet.domain.member.repository.AppConfigRepository;
+import com.i3e3.mindlet.global.constant.message.ErrorMessage;
+import com.i3e3.mindlet.global.enums.Community;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,4 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MemberServiceImpl implements MemberService {
+
+    private final AppConfigRepository appConfigRepository;
+
+    @Transactional
+    @Override
+    public void changeCommunity(Long memberSeq, Community community) {
+        AppConfig appConfig = appConfigRepository.findByMemberSeq(memberSeq)
+                .orElseThrow(() -> new IllegalStateException(ErrorMessage.INVALID_REQUEST.getMessage()));
+
+        appConfig.changeCommunity(community);
+    }
 }
