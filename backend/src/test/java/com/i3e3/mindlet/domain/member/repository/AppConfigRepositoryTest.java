9bcf33c816c010dcbd31fc48f33725d842b4fd3d
안영진 <fufru@naver.com>
2022-05-04 18:28:34 +0900

    #S06P31A106-30 Test: 앱 설정 레포지토리 메소드 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/member/repository/AppConfigRepositoryTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/member/repository/AppConfigRepositoryTest.java
@@ -3,11 +3,15 @@ package com.i3e3.mindlet.domain.member.repository;
 import com.i3e3.mindlet.domain.member.entity.AppConfig;
 import com.i3e3.mindlet.domain.member.entity.Member;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -44,5 +48,23 @@ class AppConfigRepositoryTest {
                 .member(member1)
                 .language(AppConfig.Language.ENGLISH)
                 .build();
+    }
+
+    @Test
+    @DisplayName("회원 식별키로 앱 설정 데이터 조회 - 앱 설정 데이터가 있는 경우")
+    void findAppConfigSuccess() {
+        //given
+        Member savedMember = memberRepository.save(member1);
+        AppConfig savedAppConfig = appConfigRepository.save(appConfig1);
+        em.flush();
+        em.clear();
+
+        //when
+        AppConfig findAppConfig = appConfigRepository.findByMemberSeq(savedMember.getSeq())
+                .orElse(null);
+
+        //then
+        assertThat(findAppConfig.getSeq()).isEqualTo(savedAppConfig.getSeq());
+        assertThat(findAppConfig.getMember().getSeq()).isEqualTo(savedMember.getSeq());
     }
 }
