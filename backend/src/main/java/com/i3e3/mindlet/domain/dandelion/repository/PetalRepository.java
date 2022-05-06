e9175682ab3c94dbeda66c2e4eff8e662e4c882b
Yungi <luk951202@naver.com>
2022-05-06 16:42:08 +0900

    #S06P31A106-29 Feat: 꽃잎 식별키로 민들레 데이터 조회 JPQL 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepository.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepository.java
@@ -1,5 +1,6 @@
 package com.i3e3.mindlet.domain.dandelion.repository;
 
+import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
 import com.i3e3.mindlet.domain.dandelion.entity.Petal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,7 @@ public interface PetalRepository extends JpaRepository<Petal, Long> {
 
     @Query("SELECT p FROM Petal p WHERE p.seq = :seq AND p.isDeleted = FALSE")
     Optional<Petal> findBySeq(@Param("seq") Long seq);
+
+    @Query("SELECT p.dandelion FROM Petal p WHERE p.seq = :seq AND p.isDeleted = FALSE")
+    Optional<Dandelion> findDandelionBySeq(@Param("seq") Long seq);
 }
