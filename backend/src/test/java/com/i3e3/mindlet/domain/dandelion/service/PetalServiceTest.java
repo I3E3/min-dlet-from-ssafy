edbd4bafe8a4799c373c9c5f04a7447707d1b42a
Yungi <luk951202@naver.com>
2022-05-06 17:00:44 +0900

    #S06P31A106-29 Create: 꽃잎 서비스 테스트 클래스 생성

---
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/service/PetalServiceTest.java
@@ -0,0 +1,10 @@
+package com.i3e3.mindlet.domain.dandelion.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class PetalServiceTest {
+
+}
