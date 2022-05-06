132cab2e2d7aef1329b0f82ae4d5da1a58fa7708
Yungi <luk951202@naver.com>
2022-05-06 16:41:37 +0900

    #S06P31A106-29 Feat: 꽃잎 식별키로 꽃잎데이터 조회 JPQL 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepository.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepository.java
@@ -2,7 +2,13 @@ package com.i3e3.mindlet.domain.dandelion.repository;
 
 import com.i3e3.mindlet.domain.dandelion.entity.Petal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PetalRepository extends JpaRepository<Petal, Long> {
 
+    @Query("SELECT p FROM Petal p WHERE p.seq = :seq AND p.isDeleted = FALSE")
+    Optional<Petal> findBySeq(@Param("seq") Long seq);
 }
