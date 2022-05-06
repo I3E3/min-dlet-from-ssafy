9bb790201a46bb365c54e95ed83a500768224d7c
김경석 <0xe82de@gmail.com>
2022-05-06 18:08:30 +0900

    #S06P31A106-105 Create: 회원 로그인 DTO 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/service/dto/request/MemberLoginDto.java
@@ -0,0 +1,9 @@
+package com.i3e3.mindlet.domain.member.service.dto.request;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(of = {"id", "password"})
+public class MemberLoginDto {
+}
