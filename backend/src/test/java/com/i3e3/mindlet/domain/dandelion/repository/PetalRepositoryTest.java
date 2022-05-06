5cd1090fd98e31144fda14bf43885b5b90da0a7d
Yungi <luk951202@naver.com>
2022-05-06 16:54:26 +0900

    #S06P31A106-29 Create: 꽃잎 레포지토리 테스트 클래스 생성

---
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.i3e3.mindlet.domain.dandelion.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+public class PetalRepositoryTest {
+
+}
