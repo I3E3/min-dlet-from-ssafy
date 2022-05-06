e06735402e2db5afd431ffa9e680a601b7376fee
김경석 <0xe82de@gmail.com>
2022-05-06 11:21:03 +0900

    #S06P31A106-104 Feat: 회원 식별키로 회원 엔티티 조회 레파지토리 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/member/repository/MemberRepository.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT COUNT(m.seq) > 0 FROM Member m WHERE m.seq = :seq AND m.isDeleted = false")
@@ -12,4 +14,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("SELECT COUNT(m) > 0 FROM Member m WHERE m.id = :id")
     boolean existsByIdContainsDeleted(@Param("id") String id);
+
+    @Query("SELECT m FROM Member m WHERE m.seq = :seq AND m.isDeleted = FALSE")
+    Optional<Member> findBySeq(@Param("seq") Long seq);
 }
