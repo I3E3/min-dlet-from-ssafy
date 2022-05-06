1c1bebb08d42ceb816b527b072041060dc7b3080
Yungi <luk951202@naver.com>
2022-05-06 16:39:55 +0900

    #S06P31A106-29 Create: 꽃잎 레포지토리 인터페이스 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepository.java
@@ -0,0 +1,8 @@
+package com.i3e3.mindlet.domain.dandelion.repository;
+
+import com.i3e3.mindlet.domain.dandelion.entity.Petal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetalRepository extends JpaRepository<Petal, Long> {
+
+}
