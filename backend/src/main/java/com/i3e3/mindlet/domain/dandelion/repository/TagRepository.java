41974de93649589f4bee2e0db4e52cd1a71fa897
안영진 <fufru@naver.com>
2022-05-06 09:29:17 +0900

    #S06P31A106-50 Feat: 민들레 태그 삭제 레포지토리 메소드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/TagRepository.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/TagRepository.java
@@ -3,5 +3,9 @@ package com.i3e3.mindlet.domain.dandelion.repository;
 import com.i3e3.mindlet.domain.dandelion.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findBySeq(Long seq);
+
 }
