ec084c9e04863af6fd904d0b4c854e2483aa9a54
김경석 <0xe82de@gmail.com>
2022-05-06 11:20:11 +0900

    #S06P31A106-104 Feat: 회원가입 정보 필드 및 생성자 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/member/controller/dto/RegisterRequestDto.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/controller/dto/RegisterRequestDto.java
@@ -1,9 +1,35 @@
 package com.i3e3.mindlet.domain.member.controller.dto;
 
+import com.i3e3.mindlet.domain.member.service.dto.MemberRegisterDto;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.Pattern;
+
 @Getter
-@ToString(of = {})
+@ToString(of = {"id", "password"})
 public class RegisterRequestDto {
+
+    @Schema(title = "아이디", description = "아이디입니다.", example = "good", minLength = 4, maxLength = 12, required = true)
+    @Pattern(message = "", regexp = "^[0-9|a-z|\\s]{4,12}$")
+    private String id;
+
+    @Schema(title = "패스워드", description = "패스워드입니다.", example = "test12#$", minLength = 8, maxLength = 20, required = true)
+    @Pattern(message = "", regexp = "^((?=.*[a-z])(?=.*\\d)((?=.*\\W))|(?=.*\\W)((?=.*\\d)|(?=.*[a-z]))).{8,20}$")
+    private String password;
+
+    @Builder
+    public RegisterRequestDto(String id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
+    public MemberRegisterDto toServiceDto() {
+        return MemberRegisterDto.builder()
+                .id(id)
+                .password(password)
+                .build();
+    }
 }
