52875df2d7c5087fab333cca0ea87b36f6984118
김경석 <0xe82de@gmail.com>
2022-05-06 18:19:49 +0900

    #S06P31A106-105 Feat: 회원 식별키 반환 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/global/util/AuthenticationUtil.java
+++ backend/src/main/java/com/i3e3/mindlet/global/util/AuthenticationUtil.java
@@ -1,4 +1,11 @@
 package com.i3e3.mindlet.global.util;
 
+import com.i3e3.mindlet.global.auth.CustomUserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 public class AuthenticationUtil {
+
+    public static Long getMemberSeq() {
+        return ((CustomUserDetails) SecurityContextHolder.getContext().getAuthentication().getDetails()).getMember().getSeq();
+    }
 }
