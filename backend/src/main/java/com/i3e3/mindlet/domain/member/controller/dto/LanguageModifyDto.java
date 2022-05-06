2b2db9941539dbd20e64989379f0710df44d64db
안영진 <fufru@naver.com>
2022-05-06 15:47:41 +0900

    #S06P31A106-24 Feat: 언어 변경 Dto 필드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/member/controller/dto/LanguageModifyDto.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/controller/dto/LanguageModifyDto.java
@@ -4,9 +4,14 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+
 @Schema(name = "언어 변경 DTO", description = "언어 변경 API 를 호출할 때 사용됩니다.")
 @Getter
 @ToString(of = {"language"})
 public class LanguageModifyDto {
 
+    @Schema(title = "언어", description = "사용자가 선택한 언어 데이터를 담는 필드입니다.", example = "KOREAN", required = true)
+    @NotBlank
+    private String language;
 }
