915b31d1b6a471631ba1a29f0248d7a0389a8121
김경석 <0xe82de@gmail.com>
2022-05-06 11:26:05 +0900

    #S06P31A106-104 Test: 회원 레파지토리 메서드 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/member/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/member/repository/MemberRepositoryTest.java
@@ -105,4 +105,21 @@ class MemberRepositoryTest {
         // then
         assertThat(isExists).isTrue();
     }
+
+    @Test
+    @DisplayName("회원 식별키로 회원 엔티티 조회 - 데이터가 있는 경우")
+    void findMemberBySeq() {
+        // given
+        Member savedMember = memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember = memberRepository.findBySeq(savedMember.getSeq())
+                .orElse(null);
+
+        // then
+        assertThat(findMember.getId()).isEqualTo(member1.getId());
+        assertThat(findMember.getPassword()).isEqualTo(member1.getPassword());
+    }
 }
