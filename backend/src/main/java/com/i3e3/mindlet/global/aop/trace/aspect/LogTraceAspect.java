51f0f5c9fa1fead368d02f2726eb396239384415
김경석 <0xe82de@gmail.com>
2022-05-07 11:50:42 +0900

    #S06P31A106-74 Create: 로그 트레이스 Aspect 클래 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/global/aop/trace/aspect/LogTraceAspect.java
@@ -0,0 +1,9 @@
+package com.i3e3.mindlet.global.aop.trace.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+
+@Slf4j
+@Aspect
+public class LogTraceAspect {
+}
