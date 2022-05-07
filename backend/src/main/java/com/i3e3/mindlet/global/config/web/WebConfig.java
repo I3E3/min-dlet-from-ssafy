fb5927ed0d5cf61e78c1f53d3cf7416191f69c59
김경석 <0xe82de@gmail.com>
2022-05-07 11:47:51 +0900

    #S06P31A106-74 Refactor: 로깅 인터셉터 삭제

--- backend/src/main/java/com/i3e3/mindlet/global/config/web/WebConfig.java
+++ backend/src/main/java/com/i3e3/mindlet/global/config/web/WebConfig.java
@@ -1,9 +1,7 @@
 package com.i3e3.mindlet.global.config.web;
 
-import com.i3e3.mindlet.global.interceptor.LogInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
@@ -13,14 +11,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Value("${path.saved.files}")
     private String savedFilePath;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LogInterceptor())
-                .order(1)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/error");
-    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
