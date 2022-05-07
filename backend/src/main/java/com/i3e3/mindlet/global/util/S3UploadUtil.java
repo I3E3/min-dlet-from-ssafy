313c72dc54c6bd6c726d258afa892e968447e186
김경석 <0xe82de@gmail.com>
2022-05-07 18:43:06 +0900

    #S06P31A106-108 Create: AWS S3 업로드 유틸 클래스 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/global/util/S3UploadUtil.java
@@ -0,0 +1,16 @@
+package com.i3e3.mindlet.global.util;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class S3UploadUtil {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+}
