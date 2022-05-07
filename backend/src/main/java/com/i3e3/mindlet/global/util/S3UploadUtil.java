195cd31446b51a0e8da27456195b5afe6e827aa8
김경석 <0xe82de@gmail.com>
2022-05-07 18:45:53 +0900

    #S06P31A106-108 Feat: File 객체 컨버팅 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/global/util/S3UploadUtil.java
+++ backend/src/main/java/com/i3e3/mindlet/global/util/S3UploadUtil.java
@@ -4,6 +4,12 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -13,6 +19,20 @@ public class S3UploadUtil {
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
+
+    public Optional<File> convert(MultipartFile file) throws IOException {
+        File convertFile = new File(System.getProperty("user.dir") + "/" + file.getOriginalFilename());
+
+        if (convertFile.createNewFile()) {
+            try (FileOutputStream fos = new FileOutputStream(convertFile)) {
+                fos.write(file.getBytes());
+            }
+
+            return Optional.of(convertFile);
+        }
+
+        return Optional.empty();
+    }
 
     private String extractExt(String originalFilename) {
         int pos = originalFilename.lastIndexOf(".");
