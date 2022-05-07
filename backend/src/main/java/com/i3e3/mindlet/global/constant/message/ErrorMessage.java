c4c22b8cbcd0291e94f3e86535145a54a290ac96
김경석 <0xe82de@gmail.com>
2022-05-07 18:41:02 +0900

    #S06P31A106-108 Feat: 파일 에러 메시지 추가

--- backend/src/main/java/com/i3e3/mindlet/global/constant/message/ErrorMessage.java
+++ backend/src/main/java/com/i3e3/mindlet/global/constant/message/ErrorMessage.java
@@ -7,7 +7,9 @@ public enum ErrorMessage {
     INVALID_REGISTER_KEY("유효하지 않은 회원가입 키입니다."),
     INVALID_ID("유효하지 않은 아이디입니다."),
     INVALID_REQUEST("잘못된 요청입니다."),
-    MORE_THAN_MAX_COUNT("최대 개수를 초과하였습니다.");
+    MORE_THAN_MAX_COUNT("최대 개수를 초과하였습니다."),
+    FILE_CONVERT_FAIL("파일 저장에 실패하였습니다."),
+    FILE_DELETE_FAIL("파일 삭제에 실패하였습니다.");
 
     private final String message;
 
