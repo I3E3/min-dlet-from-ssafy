ac682fb9216526cd35fd9e33ace2d4f26d90555a
Yungi <luk951202@naver.com>
2022-05-09 00:04:31 +0900

    #S06P31A106-42 Feat: 꽃밭 민들레 리스트 조회 서비스 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionService.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/DandelionService.java
@@ -1,7 +1,10 @@
 package com.i3e3.mindlet.domain.dandelion.service;
 
 import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
+import com.i3e3.mindlet.domain.dandelion.service.dto.ResponseGardenInfoDto;
 import com.i3e3.mindlet.domain.dandelion.service.dto.SeedCountDto;
+
+import java.util.List;
 
 public interface DandelionService {
 
@@ -18,4 +21,6 @@ public interface DandelionService {
     void changeStatus(Long dandelionSeq, Dandelion.Status status);
 
     void deleteTag(Long tagSeq, Long memberSeq);
+
+    List<ResponseGardenInfoDto> getGardenInfoList(Long memberSeq);
 }
