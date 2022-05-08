1e927be54f2b54f5d7d4769bf3bea5a0efea7a47
Yungi <luk951202@naver.com>
2022-05-09 00:20:29 +0900

    #S06P31A106-42 Fix: ToString 부분 오타 수정 petalSeq -> reason

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/controller/dto/ReportReasonDto.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/controller/dto/ReportReasonDto.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.NotBlank;
 
 @Schema(name = "꽃잎 신고 사유 DTO", description = "꽃잎 컨텐츠 신고 API를 호출할 때 사용됩니다.")
 @Getter
-@ToString(of = {"petalSeq"})
+@ToString(of = {"reason"})
 public class ReportReasonDto {
 
     @Schema(title = "신고 사유", description = "신고 사유를 담는 필드입니다.", example = "ALBUM")
