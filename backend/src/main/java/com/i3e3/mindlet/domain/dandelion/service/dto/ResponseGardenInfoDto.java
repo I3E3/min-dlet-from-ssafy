9fca3e03475035d6ff6932c961dee1501b9dec16
Yungi <luk951202@naver.com>
2022-05-09 00:09:19 +0900

    #S06P31A106-42 Feat: 꽃밭 민들레 정보 담을 Dto 클래스 필드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/dto/ResponseGardenInfoDto.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/dto/ResponseGardenInfoDto.java
@@ -1,5 +1,30 @@
 package com.i3e3.mindlet.domain.dandelion.service.dto;
 
-public class ResponseGardenInfoDto {
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
 
+import java.time.LocalDate;
+
+@Getter
+@ToString(of = {"blossomDate", "description", "flowerSingNumber", "seq", "status"})
+public class ResponseGardenInfoDto {
+    private LocalDate blossomDate;
+
+    private String description;
+
+    private Integer flowerSignNumber;
+
+    private Long seq;
+
+    private String status;
+
+    @Builder
+    public ResponseGardenInfoDto(LocalDate blossomDate, String description, Integer flowerSignNumber, Long seq, String status) {
+        this.blossomDate = blossomDate;
+        this.description = description;
+        this.flowerSignNumber = flowerSignNumber;
+        this.seq = seq;
+        this.status = status;
+    }
 }
