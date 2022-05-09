419dce2278fb7578f340fc423d52441907f135b5
안영진 <fufru@naver.com>
2022-05-09 21:28:11 +0900

    #S06P31A106-49 Create: 태그 서비스 인터페이스, 구현체 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/TagServiceImpl.java
@@ -0,0 +1,13 @@
+package com.i3e3.mindlet.domain.dandelion.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class TagServiceImpl implements TagService {
+}
