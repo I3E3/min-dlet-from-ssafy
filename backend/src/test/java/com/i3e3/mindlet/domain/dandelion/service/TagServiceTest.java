e0e2180f50b0023423d0f1a7d4f0866b81b56b8d
안영진 <fufru@naver.com>
2022-05-09 21:39:54 +0900

    #S06P31A106-49 Create: 태그 서비스 테스트 클래스 생성

---
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/service/TagServiceTest.java
@@ -0,0 +1,9 @@
+package com.i3e3.mindlet.domain.dandelion.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class TagServiceTest {
+}
