583b66043c008713e932a1fc8cc076256aeeaf4c
안영진 <fufru@naver.com>
2022-05-09 21:35:14 +0900

    #S06P31A106-49 Test: 꽃잎 레포지토리 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepositoryTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/repository/PetalRepositoryTest.java
@@ -2,6 +2,7 @@ package com.i3e3.mindlet.domain.dandelion.repository;
 
 import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
 import com.i3e3.mindlet.domain.dandelion.entity.Petal;
+import com.i3e3.mindlet.domain.dandelion.service.DandelionService;
 import com.i3e3.mindlet.domain.member.entity.Member;
 import com.i3e3.mindlet.domain.member.repository.MemberRepository;
 import com.i3e3.mindlet.global.enums.Community;
@@ -33,7 +34,10 @@ public class PetalRepositoryTest {
     @Autowired
     private PetalRepository petalRepository;
 
-    private Member member1;
+    @Autowired
+    private DandelionService dandelionService;
+
+    private Member member1, member2, member3;
 
     private Dandelion dandelion1;
 
@@ -49,6 +53,16 @@ public class PetalRepositoryTest {
         member1 = Member.builder()
                 .id("아이디1")
                 .password("패스워드1")
+                .build();
+
+        member2 = Member.builder()
+                .id("아이디2")
+                .password("패스워드2")
+                .build();
+
+        member3 = Member.builder()
+                .id("아이디3")
+                .password("패스워드3")
                 .build();
 
         dandelion1 = Dandelion.builder()
@@ -178,5 +192,36 @@ public class PetalRepositoryTest {
 
         // then
         assertThat(findDandelion).isNull();
+    }
+
+    @Test
+    @DisplayName("민들레 식별키와 회원 식별키로 꽃잎 조회 - 성공")
+    void existPetalByDandelionSeqAndMemberSeqTrue() {
+        //given
+        memberRepository.save(member1);
+        Member savedMember2 = memberRepository.save(member2);
+        Dandelion savedDandelion1 = dandelionRepository.save(dandelion1);
+        petalRepository.save(petal1);
+
+        petalRepository.save(Petal.builder()
+                .message("hello2")
+                .imagePath("imagePath")
+                .nation("CANADA")
+                .city("OTTAWA")
+                .nationalFlagImagePath("nationalFlagImagePath2")
+                .dandelion(savedDandelion1)
+                .member(savedMember2)
+                .build());
+
+        em.flush();
+        em.clear();
+
+        //when
+
+        boolean isParticipated = petalRepository.existsPetalByDandelionSeqAndMemberSeq(savedDandelion1.getSeq(), savedMember2.getSeq());
+
+        //then
+        assertThat(isParticipated).isTrue();
+
     }
 }
