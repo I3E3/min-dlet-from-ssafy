8d79ae5056ae7d6373289d6252e85c4cac794ef4
김경석 <0xe82de@gmail.com>
2022-05-10 14:46:58 +0900

    #S06P31A106-27 Feat: 메일 10분 마다 HOLD 상태인 민들레를 체크하여 최종 수정일이 경과 시간보다 오래되었으면 FLYING 상태로 변경하는 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/global/util/SchedulerUtil.java
+++ backend/src/main/java/com/i3e3/mindlet/global/util/SchedulerUtil.java
@@ -1,11 +1,23 @@
 package com.i3e3.mindlet.global.util;
 
+import com.i3e3.mindlet.domain.dandelion.repository.DandelionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 @EnableScheduling
 public class SchedulerUtil {
+
+    private final DandelionRepository dandelionRepository;
+
+    @Transactional
+    @Scheduled(cron = "0 0/10 * * * *")
+    public void checkHoldDandelion() {
+        long elapsedMinute = 30L;
+        dandelionRepository.updateHoldingDandelionToFlying(elapsedMinute);
+    }
 }
