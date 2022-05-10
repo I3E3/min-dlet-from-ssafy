447e3535118ab123ad6ea9a1977fda2506096cdd
김경석 <0xe82de@gmail.com>
2022-05-10 14:44:09 +0900

    #S06P31A106-27 Feat: 랜덤 Flying 민들레씨 1개 데이터 조회 메서드 추가
    - 최종 수정일이 가장 오래된 민들레 엔티티를 반환합니다.

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepositoryCustom.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepositoryCustom.java
@@ -1,12 +1,16 @@
 package com.i3e3.mindlet.domain.dandelion.repository;
 
 import com.i3e3.mindlet.domain.dandelion.entity.Dandelion;
+import com.i3e3.mindlet.domain.member.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DandelionRepositoryCustom {
 
     int countUsingSeed(Long memberSeq);
 
     List<Dandelion> findDandelionListByMemberSeq(Long memberSeq);
+
+    Optional<Dandelion> findRandomFlyingDandelionExceptMember(Member member);
 }
