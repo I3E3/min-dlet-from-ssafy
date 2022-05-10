45f0be12c5f33cd7f25979addd3e247d2b55187e
김경석 <0xe82de@gmail.com>
2022-05-10 14:45:59 +0900

    #S06P31A106-27 Create: 스케줄러 유틸 클래스 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/global/util/SchedulerUtil.java
@@ -0,0 +1,11 @@
+package com.i3e3.mindlet.global.util;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@EnableScheduling
+public class SchedulerUtil {
+}
