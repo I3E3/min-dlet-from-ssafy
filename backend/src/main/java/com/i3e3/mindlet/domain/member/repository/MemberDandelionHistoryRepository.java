5ab4188e551ea3ee00e1ae879d000f98115bb003
김경석 <0xe82de@gmail.com>
2022-05-10 14:48:33 +0900

    #S06P31A106-27 Create: 회원 민들레 조회 이력 레포지토리 클래스 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/member/repository/MemberDandelionHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.i3e3.mindlet.domain.member.repository;
+
+import com.i3e3.mindlet.domain.member.entity.MemberDandelionHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberDandelionHistoryRepository extends JpaRepository<MemberDandelionHistory, Long> {
+}
