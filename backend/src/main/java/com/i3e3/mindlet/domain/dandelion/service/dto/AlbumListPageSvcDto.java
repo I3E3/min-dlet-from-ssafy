2736e87c1cb104f82526034b40d2489724e6b917
Yungi <luk951202@naver.com>
2022-05-11 11:49:51 +0900

    #S06P31A106-47 Feat: 앨범 페이지 정보 반환 Dto 클래스 필드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/dto/AlbumListPageSvcDto.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/dto/AlbumListPageSvcDto.java
@@ -1,5 +1,47 @@
 package com.i3e3.mindlet.domain.dandelion.service.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@ToString(of = {"totalDandelionCount", "nowPageNum", "totalPageNum", "dandelionInfos"})
 public class AlbumListPageSvcDto {
 
+    private Long totalDandelionCount;
+
+    private int nowPageNum;
+
+    private int totalPageNum;
+
+    List<AlbumListPageSvcDto.dandelionInfo> dandelionInfos;
+
+    @Builder
+    public AlbumListPageSvcDto(Long totalDandelionCount,
+                                int nowPageNum,
+                                int totalPageNum,
+                                List<AlbumListPageSvcDto.dandelionInfo> dandelionInfos) {
+
+        this.totalDandelionCount = totalDandelionCount;
+        this.nowPageNum = nowPageNum;
+        this.totalPageNum = totalPageNum;
+        this.dandelionInfos = dandelionInfos;
+    }
+
+    @Getter
+    @ToString(of = {"dandelionSeq", "description"})
+    public static class dandelionInfo {
+
+        private Long DandelionSeq;
+
+        private String description;
+
+        @Builder
+        public dandelionInfo(Long dandelionSeq, String description) {
+            this.DandelionSeq = dandelionSeq;
+            this.description = description;
+        }
+    }
 }
