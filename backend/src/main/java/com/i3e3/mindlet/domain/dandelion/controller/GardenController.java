036f44f902fe72bc8a444fd49e98f66a33f784e2
Yungi <luk951202@naver.com>
2022-05-11 11:43:48 +0900

    #S06P31A106-47 Feat: 앨범 조회 API 메소드 추가

--- backend/src/main/java/com/i3e3/mindlet/domain/dandelion/controller/GardenController.java
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/controller/GardenController.java
@@ -1,7 +1,9 @@
 package com.i3e3.mindlet.domain.dandelion.controller;
 
 import com.i3e3.mindlet.domain.dandelion.service.DandelionService;
+import com.i3e3.mindlet.domain.dandelion.service.dto.AlbumListPageSvcDto;
 import com.i3e3.mindlet.domain.dandelion.service.dto.ResponseGardenInfoDto;
+import com.i3e3.mindlet.global.constant.message.ErrorMessage;
 import com.i3e3.mindlet.global.dto.BaseResponseDto;
 import com.i3e3.mindlet.global.dto.ErrorResponseDto;
 import com.i3e3.mindlet.global.util.AuthenticationUtil;
@@ -12,7 +14,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +30,8 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/garden")
 public class GardenController {
-    DandelionService dandelionService;
+
+    private final DandelionService dandelionService;
 
     @Operation(
             summary = "꽃밭 조회 API 기능 추가",
@@ -63,5 +68,57 @@ public class GardenController {
         return BaseResponseDto.<List<ResponseGardenInfoDto>>builder()
                 .data(gardenInfoDtoListList)
                 .build();
+    }
+
+    @Operation(
+            summary = "앨범 조회 API 기능 추가",
+            description = "인증 토큰, 페이지 번호, 사이즈를 전달받고 꽃밭에 심어진 민들레 정보 리스트를 반환합니다.",
+            tags = {"dandelion"}
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "조회 성공",
+                    content = @Content(schema = @Schema(implementation = BaseResponseDto.class))),
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "데이터 없음",
+                    content = @Content(schema = @Schema(implementation = BaseResponseDto.class))),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "토큰 검증 실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "권한 검증 실패",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class))),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "서버 에러",
+                    content = @Content(schema = @Schema(implementation = ErrorResponseDto.class)))
+    })
+    @PreAuthorize("hasAnyRole('ROLE_MEMBER')")
+    @GetMapping("/album")
+    public ResponseEntity<BaseResponseDto<AlbumListPageSvcDto>> albumList(Pageable pageable) {
+
+        if (pageable.getPageNumber() < 1 || pageable.getPageSize() > 20 || pageable.getPageSize() < 1) {
+            throw new IllegalStateException(ErrorMessage.INVALID_REQUEST.getMessage());
+        }
+
+        Long memberSeq = AuthenticationUtil.getMemberSeq();
+
+        AlbumListPageSvcDto albumListPageSvcDto = dandelionService.getAlbumInfo(memberSeq, pageable.getPageNumber(), pageable.getPageSize());
+
+        HttpStatus status = null;
+
+        if (albumListPageSvcDto == null) {
+            status = HttpStatus.NO_CONTENT;
+        } else {
+            status = HttpStatus.OK;
+        }
+
+        return new ResponseEntity<>(BaseResponseDto.<AlbumListPageSvcDto>builder()
+                .data(albumListPageSvcDto)
+                .build(), status);
     }
 }
