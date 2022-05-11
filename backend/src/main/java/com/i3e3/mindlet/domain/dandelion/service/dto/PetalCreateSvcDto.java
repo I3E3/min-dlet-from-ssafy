37669417ef82bc7ef39bdbf76bc672041d314422
안영진 <fufru@naver.com>
2022-05-11 16:43:34 +0900

    #S06P31A106-26 Create: 꽃잎 생성 서비스 Dto 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/service/dto/PetalCreateSvcDto.java
@@ -0,0 +1,10 @@
+package com.i3e3.mindlet.domain.dandelion.service.dto;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString(of = {"message", "imageFile", "nation"})
+public class PetalCreateSvcDto {
+
+}
