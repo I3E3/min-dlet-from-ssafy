7eab26fca1e3f7f5cd88bf5d788e58a0a4780340
김경석 <0xe82de@gmail.com>
2022-05-12 18:22:41 +0900

    #S06P31A106-39 Test: 회원 식별키로 회원 엔티티(삭제 포함) 조회 레포지토리 메서드 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/member/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/member/repository/MemberRepositoryTest.java
@@ -212,4 +212,21 @@ class MemberRepositoryTest {
         // then
         assertThat(findMember1).isNull();
     }
+
+    @Test
+    @DisplayName("회원 식별키로 회원 엔티티(삭제 포함) 조회 - 데이터가 있는 경우")
+    void findMemberBySeqContainsDeletedWhenExist() {
+        // given
+        Member savedMember1 = memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        // when
+        Member findMember1 = memberRepository.findBySeqContainsDeleted(savedMember1.getSeq())
+                .orElse(null);
+
+        // then
+        assertThat(findMember1.getSeq()).isEqualTo(savedMember1.getSeq());
+        assertThat(findMember1.isDeleted()).isFalse();
+    }
 }
