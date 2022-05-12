fafe6851664e7ae34f0712f8374f60a2500adfa3
김경석 <0xe82de@gmail.com>
2022-05-12 14:31:14 +0900

    #S06P31A106-28 Create: 꽃잎 추가 요청 DTO 생성

---
+++ backend/src/main/java/com/i3e3/mindlet/domain/dandelion/controller/dto/PetalRegisterDto.java
@@ -0,0 +1,11 @@
+package com.i3e3.mindlet.domain.dandelion.controller.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.ToString;
+
+@Schema(name = "꽃잎 추가 요청 DTO", description = "꽃잎 추가 API 를 호출할 때 사용됩니다.")
+@Getter
+@ToString(of = {})
+public class PetalRegisterDto {
+}
