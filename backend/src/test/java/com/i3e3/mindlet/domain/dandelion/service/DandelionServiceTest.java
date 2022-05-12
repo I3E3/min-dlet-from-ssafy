47463fe0b05a330acf3ad672bf4c0378b77971d8
김경석 <0xe82de@gmail.com>
2022-05-12 14:46:28 +0900

    #S06P31A106-28 Test: 꽃잎 추가 서비스 메서드 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/service/DandelionServiceTest.java
@@ -1883,4 +1883,25 @@ class DandelionServiceTest {
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessage(ErrorMessage.INVALID_REQUEST.getMessage());
     }
+
+    @Test
+    @DisplayName("꽃잎 추가 - 예외 발생 : 민들레 삭제 처리")
+    void addPetalExceptionWhenDeletedDandelion() {
+        // given
+        Member savedMember1 = memberRepository.save(member1);
+        dandelion1.delete();
+        Dandelion savedDandelion1 = dandelionRepository.save(dandelion1);
+        PetalCreateSvcDto newPetalCreateSvcDto = PetalCreateSvcDto.builder()
+                .message("하하하하하하하")
+                .imageFile(null)
+                .nation("KOREA")
+                .build();
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> dandelionService.addPetal(savedMember1.getSeq(), savedDandelion1.getSeq(), newPetalCreateSvcDto))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage(ErrorMessage.INVALID_REQUEST.getMessage());
+    }
 }
