dec6ad319c247851cc10304858766dfeb33de46c
limgeonho <ghlim909@gmail.com>
2022-05-12 11:01:29 +0900

    #S06P22A302-86 Feat: 기록 보관함 컴포넌트 작성

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { RecoilRoot } from 'recoil';
-import Router from 'routes/routes';
-import { BrowserView, MobileView } from 'react-device-detect';
-import LoginRecoil from 'utils/LoginRecoil';
-import { Toaster } from 'react-hot-toast';
+import React from "react";
+import { RecoilRoot } from "recoil";
+import Router from "routes/routes";
+import { BrowserView, MobileView } from "react-device-detect";
+import LoginRecoil from "utils/LoginRecoil";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <div>
       <RecoilRoot>
         <Toaster position="top-center" />
-        <LoginRecoil />
+        {/* <LoginRecoil /> */}
         <BrowserView>
           <h1> 모바일에 최적화 된 페이지 입니다. 모바일로 접속해주세요! </h1>
         </BrowserView>
--- frontend/src/services/api/MyGardenApi.js
+++ frontend/src/services/api/MyGardenApi.js
@@ -1,5 +1,5 @@
 const BASE_PATH = "http://localhost:8080";
-const SERVER = "http://k6a106.p.ssafy.io:8080";
+const SERVER = "https://www.min-dlet.com/login";
 
 // 꽃밭 보기
 export function getGarden() {
