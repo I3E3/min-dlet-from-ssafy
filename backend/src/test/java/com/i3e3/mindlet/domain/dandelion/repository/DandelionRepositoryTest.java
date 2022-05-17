7938d3ee77cadb5db4b06160d0f9038feb5c6469
김경석 <0xe82de@gmail.com>
2022-05-17 11:25:09 +0900

    #S06P31A106-137 Test: 민들레 READY 상태로 변경 레포지토리 메서드 테스트 케이스 추가

--- backend/src/test/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepositoryTest.java
+++ backend/src/test/java/com/i3e3/mindlet/domain/dandelion/repository/DandelionRepositoryTest.java
@@ -1372,4 +1372,26 @@ class DandelionRepositoryTest {
         assertThat(findDandelion1.getMember().getSeq()).isEqualTo(savedDandelion1.getMember().getSeq());
         assertThat(findDandelion1.getStatus()).isEqualTo(savedDandelion1.getStatus());
     }
+
+    @Test
+    @DisplayName("민들레 READY 상태로 변경 - 변화 없음(From HOLD And Deleted)")
+    void updateDandelionStatusToReadySuccessFromHoldAndDeleted() {
+        // given
+        memberRepository.save(member1);
+        dandelion1.delete();
+        dandelion1.changeStatus(Dandelion.Status.HOLD);
+        Dandelion savedDandelion1 = dandelionRepository.save(dandelion1);
+        em.flush();
+        em.clear();
+
+        // when
+        dandelionRepository.updateFlyingOrHoldingDandelionToReady();
+        Dandelion findDandelion1 = dandelionRepository.findBySeqContainsDeleted(savedDandelion1.getSeq())
+                .orElse(null);
+
+        // then
+        assertThat(findDandelion1.getSeq()).isEqualTo(savedDandelion1.getSeq());
+        assertThat(findDandelion1.getMember().getSeq()).isEqualTo(savedDandelion1.getMember().getSeq());
+        assertThat(findDandelion1.getStatus()).isEqualTo(savedDandelion1.getStatus());
+    }
 }
