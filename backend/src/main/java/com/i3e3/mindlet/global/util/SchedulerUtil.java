57906e6130dcb0ee7fdead5373fd6dad48f5d03a
김경석 <0xe82de@gmail.com>
2022-05-17 11:23:43 +0900

    #S06P31A106-137 Feat: 민들레 상태(READY) 변경 스케줄러 메서드 추가

--- backend/src/main/java/com/i3e3/mindlet/global/util/SchedulerUtil.java
+++ backend/src/main/java/com/i3e3/mindlet/global/util/SchedulerUtil.java
@@ -20,4 +20,10 @@ public class SchedulerUtil {
         long elapsedMinute = 30L;
         dandelionRepository.updateHoldingDandelionToFlying(elapsedMinute);
     }
+
+    @Transactional
+    @Scheduled(cron = "0 25 23 * * *")
+    public void readyDandelion() {
+        dandelionRepository.updateFlyingOrHoldingDandelionToReady();
+    }
 }
